e4107755ddc83b43e88c9da5f7813aecdb258dbe
MasoudSohrabi <111013857+MasoudSohrabi@users.noreply.github.com>
2025-02-14 10:54:29 -0800

    my first try for converting
    my first basic project

---
+++ convertor basic lesson 10-11.py
@@ -0,0 +1,8 @@
+print("how many Dollors do you want to convert toman?")
+
+dollars = float(input("Enter amount: "))
+
+toman = int(dollars) * 92500
+
+
+print(f"{dollars:,} dollars is {toman:,} Tomans")
---
+++ input data lesson 1 - 10.py
@@ -0,0 +1,27 @@
+# # prj1
+# print(3 + 4.2)
+# print(92000 * 3000)
+
+# print(3 * ' masoud  ') 
+# print(4 + 4 - 6)
+
+# print('---------------------')
+# prj2
+
+mahsoolName = 'Badoom'
+
+mahsoolPrice = 200
+
+mahsoolavalible = True
+
+mahsoolTag = ['badoom derakhti', 'badoom zamini' , 'kharid badoom']
+
+mahsool = {
+    "mahsoolName" : "Badoom"  ,
+     "mahsoolPrice" : 200 ,
+   
+       }
+
+mahsoolOmde = None
+
+print(mahsool)
